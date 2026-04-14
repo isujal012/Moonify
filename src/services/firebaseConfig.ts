2fd4dee201c547889bfc24d598671124abb70011
Sujal <isujal012@users.noreply.github.com>
2026-04-14 22:31:07 +0530

    Move Firebase secrets to .env file

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCi2jG4z0M_3A4Q_sokk8m1VVbEEgnKp5w",
-  authDomain: "moonify-8e6eb.firebaseapp.com",
-  projectId: "moonify-8e6eb",
-  storageBucket: "moonify-8e6eb.firebasestorage.app",
-  messagingSenderId: "16409978311",
-  appId: "1:16409978311:web:ffeb3baee8f39de38ae5e2",
-  measurementId: "G-000XPFC3CJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || ""
 };
 
 const app = initializeApp(firebaseConfig);
